07b31a2ac71288bc17a611c083a6c6977231ee2b
Joel Santos <joeloliveira56@gmail.com>
2024-01-22 15:44:55 -0300

    Update alunoRoutes.js

--- nodeBD 16-01/routes/alunoRoutes.js
+++ nodeBD 16-01/routes/alunoRoutes.js
@@ -8,5 +8,8 @@ router.get('/listarAlunos', alunoController.listarAlunos)
 router.get('/buscarAluno/:matricula', alunoController.buscarAluno)
 router.get('/buscarAluno/:', alunoController.buscarAlunoTurma)
 router.delete('/deletarAluno/:matricula', alunoController.deletarAluno)
+router.put('/editarAluno/:matricula', alunoController.editarAluno)
+router.get('/cadastroAluno', alunoController.formCadastro)
 
 module.exports = router
+
